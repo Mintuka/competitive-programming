858316d1de622251e1f144242e3ad9bdcba454b2
mintuka <mintuka2015@gmail.com>
2023-02-02 21:16:07 -0800

    Integer to English Words

--- squid_game/week_1_day_2/Integer to English Words.py
+++ squid_game/week_1_day_2/Integer to English Words.py
@@ -1,5 +1,5 @@
-class Solution(object):
-    def numberToWords(self, num):
+class Solution:
+    def numberToWords(self, num) -> str:
         
         map_digits = {0:"Zero",1:"One",2:"Two",3:"Three",4:"Four",5:"Five",6:"Six",7:"Seven",8:"Eight",9:"Nine",10:"Ten",
                      11:"Eleven",12:"Twelve",13:"Thirteen",14:"Fourteen",15:"Fifteen",16:"Sixteen",17:"Seventeen",
@@ -9,10 +9,10 @@ class Solution(object):
         if num == 0:
             return 'Zero'
 
-        result = []
-        level,factor = 0,3
         curr_num = num//1000
         curr_mod = num%1000
+        result = []
+        level,factor = 0,3
         
         while curr_num or curr_mod:
             if curr_mod and level:
@@ -25,7 +25,8 @@ class Solution(object):
                     div = div // 100
                     i += 2
                 else:
-                    div, mod = divmod(div, 10
+                    mod = div%10
+                    div //= 10
                     if mod:
                         if not i:
                             result.append(map_digits[mod])
@@ -36,7 +37,10 @@ class Solution(object):
                             result.append(map_digits[mod])
                     i += 1
 
-            curr_num, curr_mod = divmod(curr_num, 1000)
+            curr_mod = curr_num%1000
+            curr_num //= 1000
             level += 1
 
-        return " ".join(reversed(result))
+        result = " ".join([result[i] for i in range(len(result)-1,-1,-1)])
+        return result
+        
