dc5acc3693c5121c83fef9e23b96b795e33a8ba2
mintuka <mintuka2015@gmail.com>
2023-02-16 17:10:07 +0300

    Find Original Array from Doubled Array

---
+++ squid_game/week_3_day_4/Find Original Array From Doubled Array.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        
+        unique = sorted(set(changed))
+        count = Counter(changed)
+        original = []
+        if 0 in unique and count[0]%2:
+            return []
+
+        for num in unique:
+            doubled = num*2
+            
+
+            while doubled in count and num in count:
+                count[doubled] -= 1
+                count[num] -= 1
+                original.append(num)
+                if count[num] == 0:
+                    del count[num]
+                if count[doubled] == 0:
+                    del count[doubled]
+            
+            if num in count:
+                return []
+                
+        return original
