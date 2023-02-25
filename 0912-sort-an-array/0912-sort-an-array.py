02a750b78f3d6efd5ec953fba85a7e98c04a78f7
Mintuka <59524273+Mintuka@users.noreply.github.com>
2023-02-25 16:01:47 +0300

    Time: 1897 ms (51.75%), Space: 24 MB (16.87%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -4,24 +4,31 @@ class Solution:
         mn = abs(min(nums))
         for i in range(len(nums)):
             nums[i] += mn
+        # print(nums)
+        mx = max(1,abs(max(nums)))
+        digits = floor(math.log(mx,10)) + 1
         
-        size = abs(max(nums)) + mn + 1
-        count = [0 for _ in range(size)]
+        for d in range(digits):
+            freq = [0 for _ in range(10)]
+            for num in nums:
+                div = num//(10**(d))
+                mod = div%10   
+                freq[mod] += 1
+            # print(freq)
+            for i in range(1,10):
+                freq[i] += freq[i-1]
+                
+            temp = [0 for i in range(len(nums))]
+            for idx,num in enumerate(nums[::-1]):
+                div = num//(10**(d))
+                mod = div%10
+                temp[freq[mod]-1] = num
+                freq[mod] -= 1
+            # print('temp',temp)    
+            nums = [val for val in temp]
         
-        for num in nums:
-            new_mn = num
-            count[new_mn] += 1
-         
-        for i in range(1,len(count)):
-            count[i] += count[i-1]
-        
-        res = [0 for i in range(len(nums))]
-        
-        for idx,val in enumerate(nums):
-            res[count[val]-1] = val
-            count[val] -= 1
-        
-        for i in range(len(res)):
-            res[i] -= mn
+        # print(nums)
+        for i in range(len(nums)):
+            nums[i] -= mn
             
-        return res
+        return nums
